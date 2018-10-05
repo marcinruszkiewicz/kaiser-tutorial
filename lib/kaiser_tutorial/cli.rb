b55af35d61a5f73334cd36725fabcfe72af9e5f8
Marcin Ruszkiewicz <marcin.ruszkiewicz@polcode.net>
2018-10-06 01:29:48 +0200

    adding a CLI to execute programs

---
+++ lib/kaiser_tutorial/cli.rb
@@ -0,0 +1,14 @@
+require 'thor'
+
+module KaiserTutorial
+  class CLI < Thor
+    desc "execute FILE", "transpiles and runs a .rock FILE"
+    def execute(filename)
+      file = File.read filename
+      output = KaiserTutorial.transpile(file)
+
+      instance_eval output
+      say
+    end
+  end
+end
